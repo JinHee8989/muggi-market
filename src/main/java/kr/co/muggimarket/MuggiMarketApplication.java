278ff713b1abe79a6b4369aaf53128d6dea6411f
JinHee8989 <wlsgml8989@gmail.com>
2021-11-14 15:26:56 +0900

    first commit

---
+++ src/main/java/kr/co/muggimarket/MuggiMarketApplication.java
@@ -0,0 +1,13 @@
+package kr.co.muggimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuggiMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuggiMarketApplication.class, args);
+    }
+
+}
