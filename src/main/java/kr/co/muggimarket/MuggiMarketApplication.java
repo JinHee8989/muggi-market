68fcf037ae08bf56e09fba18e987104a48cf0ff4
jinheebyeon <wlsgml8989@gmail.com>
2021-12-18 23:28:43 +0900

    커밋

--- src/main/java/kr/co/muggimarket/MuggiMarketApplication.java
+++ src/main/java/kr/co/muggimarket/MuggiMarketApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MuggiMarketApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MuggiMarketApplication.class, args);
     }
